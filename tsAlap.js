18ec3827d8ce789dd0de8c011da86ffbfbe72122
Serakg <serakg0@gmail.com>
2022-04-19 20:14:27 +0200

    TS frissítve

--- tsAlap.js
+++ tsAlap.js
@@ -1,2 +1,12 @@
-console.log("Szeretem a Typescript-et!");
-console.log("Nagyon szeretem");
+// hatáskör valtozoNev :tipus = érték;
+var szam = 5;
+var szoveg = "Szeretem a programozást";
+var logikai = false; //vagy true értéket vehet fel
+//Speciáis változók
+// :any
+var barmi = "alma";
+barmi = 5;
+barmi = true;
+var ismeretlen = "alma";
+ismeretlen = 5;
+ismeretlen = true;
--- tsAlap.ts
+++ tsAlap.ts
@@ -1,2 +1,23 @@
-console.log("Szeretem a Typescript-et!")
-console.log("Nagyon szeretem")
+// hatáskör valtozoNev :tipus = érték;
+
+var szam:number = 5;
+var szoveg:string = "Szeretem a programozást";
+var logikai:boolean = false; //vagy true értéket vehet fel
+
+//Speciáis változók
+
+// :any
+
+var barmi:any = "alma";
+barmi = 5;
+barmi=true;
+document.write(barmi);
+
+var ismeretlen:unknown = "alma"
+ismeretlen = 5;
+ismeretlen=true;
+document.write(String(ismeretlen));
+
+var szovegesTomb: string[] =["alma", "körte", "szilva", "barack"];
+var szovegesTomb2: Array<string> = ["fps", "tps", "rps", "simulator"];
+var szamTomb: Array<number> = [3, 7, 13, 21, 42];
