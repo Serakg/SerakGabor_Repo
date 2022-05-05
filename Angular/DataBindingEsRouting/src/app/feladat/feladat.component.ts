f180e5b48eeb5e4c5b9001c414bfa739118ff7fe
Serakg <serakg0@gmail.com>
2022-05-05 19:50:42 +0200

    Feladatok frissítve

--- Angular/DataBindingEsRouting/src/app/feladat/feladat.component.ts
+++ Angular/DataBindingEsRouting/src/app/feladat/feladat.component.ts
@@ -12,4 +12,16 @@ export class FeladatComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  kepHelye:string = "https://cdn.freebiesupply.com/logos/large/2x/angular-icon-logo-png-transparent.png";
+  kepCime:string = "Angular Logo";
+  kepMeret:string = "100";
+
+
+  kepModosito():void{
+    this.kepHelye = "https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg";
+    this.kepCime = "React Logo";
+    this.kepMeret = "200";
+  }
+
 }
