66a7dd0842363b67730d0b20ce3410b5cf73c50e
Serakg <serakg0@gmail.com>
2022-05-26 19:52:27 +0200

    Reactive form (űrlap) és táblázat frissítve

--- Angular/Material/src/app/app-routing.module.ts
+++ Angular/Material/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UrlapComponent } from './urlap/urlap.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"urlap", component: UrlapComponent},
+  {path:"**", component: UrlapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/Material/src/app/app.module.ts
+++ Angular/Material/src/app/app.module.ts
@@ -4,15 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UrlapComponent } from './urlap/urlap.component';
+
+import{MatInputModule} from "@angular/material/input";
+import{MatFormFieldModule} from "@angular/material/form-field";
+import{MatButtonModule} from "@angular/material/button";
+import{MatSelectModule} from "@angular/material/select";
+import{MatTableModule} from "@angular/material/table";
+import{MatIconModule} from "@angular/material/icon";
+import{MatCheckboxModule} from "@angular/material/checkbox";
+import{MatMenuModule} from "@angular/material/menu";
+import{MatGridListModule} from "@angular/material/grid-list";
+import{MatListModule} from "@angular/material/list";
+import{MatRadioModule} from "@angular/material/radio";
+import { FormsModule } from '@angular/forms';
+import{ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatListModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
