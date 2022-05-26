f0aff5dfd02468426d4dde21c4f9168723d57e5c
Serakg <serakg0@gmail.com>
2022-05-26 17:31:42 +0200

    Material frissítve

--- Angular/Material/src/app/app.module.ts
+++ Angular/Material/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TablaComponent } from './tabla/tabla.component';
-import { EletciklusComponent } from './eletciklus/eletciklus.component';
-import { JqueryComponent } from './jquery/jquery.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TablaComponent,
-    EletciklusComponent,
-    JqueryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
